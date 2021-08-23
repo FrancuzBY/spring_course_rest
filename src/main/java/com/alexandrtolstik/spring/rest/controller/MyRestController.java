00eed19314373d1cbe67e888c8afdb731d8ed251
Alexandr <francuzby@gmail.com>
2021-08-23 23:56:49 +0300

    Exception + Add employee

--- src/main/java/com/alexandrtolstik/spring/rest/controller/MyRestController.java
+++ src/main/java/com/alexandrtolstik/spring/rest/controller/MyRestController.java
@@ -1,8 +1,44 @@
 package com.alexandrtolstik.spring.rest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.alexandrtolstik.spring.rest.entity.Employee;
+import com.alexandrtolstik.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.alexandrtolstik.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.alexandrtolstik.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+
 
 }
